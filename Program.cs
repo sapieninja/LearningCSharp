6a8ca7ff717832a0477644966c78b4b4a7d19241
sapieninja <60101890+sapieninja@users.noreply.github.com>
2020-09-17 15:27:29 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace selection_max
+{
+    class Program
+    {   
+        static void isequal(int a,int b)
+        {
+        Console.WriteLine(Convert.ToString(a==b));
+        }
+        static void iseven(int a)
+        {
+        Console.WriteLine(Convert.ToString(Convert.ToBoolean(a%2+1)));
+        }
+        static bool leap(int a)
+        {
+        if (a%4==0)
+        {
+            if(a%100!=0)
+            {
+                if(a%400==0)
+                    {
+                    return true;
+                    }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else{
+            return false;
+        }
+        }
+        static int max(int a,int b,int c)
+        {
+        if(a>b && b > c){
+            return a;
+        }
+        else if(b > a && b > c){
+            return b;
+        }
+        else{
+            return c;
+        }
+        }
+        static void Main(string[] args)
+        {
+        isequal(1,1);
+        iseven(3);
+        Console.WriteLine(Convert.ToString(leap(2000)));
+        Console.WriteLine(max(1,40,29));
+        }
+    }
+}
