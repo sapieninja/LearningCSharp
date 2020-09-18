f0bbd0f48990c11dff94cf344db3a05240838c8d
sapieninja <60101890+sapieninja@users.noreply.github.com>
2020-09-18 11:49:00 +0100

    New Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Function_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
