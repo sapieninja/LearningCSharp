e0d17f28b0197849b62a305a37422b7181d5f5c7
sapieninja <maxjbowman@gmail.com>
2020-09-27 18:56:26 +0100

    Setup

---
+++ Normalisation.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Normalisation.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
