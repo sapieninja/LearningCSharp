a7784b253f9662c6577997187866ecacc75890cd
sapieninja <maxjbowman@gmail.com>
2020-10-01 12:05:36 +0100

    Setup

---
+++ ListExercises.tests/ListTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ListExercises.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ListExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
