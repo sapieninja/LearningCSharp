9cb5a166d2cd07ef252bb825a76df923f40a5d96
sapieninja <maxjbowman@gmail.com>
2020-10-15 15:37:50 +0100

    Supports negative numbers up to -128

--- Question6.tests/Question6Tests.cs
+++ Question6.tests/Question6Tests.cs
@@ -9,6 +9,7 @@ namespace Question6.tests
         public void TestMethod1()
         {  
             Assert.AreEqual(Program.toBinary("210"),"11010010");
+            Assert.AreEqual(Program.toBinary("-1"),"11111111");
         }
     }
 }
--- Question6/Program.cs
+++ Question6/Program.cs
@@ -1,11 +1,12 @@
-﻿using System.Diagnostics.Contracts;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
 using System;
 using System.Collections.Generic;
 namespace Question6
 {
     public class Program
     {
-        public static string toBinary(string convertBinary)
+        public static string positiveToBinary(string convertBinary)
         {
             int toConvert = Convert.ToInt32(convertBinary);
             var digits = new List<int>();
@@ -20,6 +21,32 @@ namespace Question6
                 output = Convert.ToString(element) + output;
             }
             return output;
+        }
+        public static string toBinary(string convertBinary)
+        {
+            int toConvert = Convert.ToInt32(convertBinary);
+            if(toConvert>=0)
+            {
+                string positivePart = positiveToBinary(Convert.ToString(toConvert));
+                int noZeros = 8-positivePart.Length;
+                string zeros = "";
+                for(int i = 0;i<noZeros;i++) 
+                {
+                    zeros += "0";
+                }
+                return zeros + positivePart;
+            }
+            else
+            {
+                string negativePart = positiveToBinary(Convert.ToString(toConvert+128));
+                int noOnes = 8-negativePart.Length;
+                string ones = "";
+                for(int i = 0;i<noOnes;i++) 
+                {
+                    ones += "1";
+                }
+                return ones + negativePart;
+            }
         }
         static void Main()
         {   
